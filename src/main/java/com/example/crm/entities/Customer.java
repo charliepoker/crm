bb89c60c8a3e06b1822cba644f0116e9df5090c9
charliepoker <iheanachocharlie@gmail.com>
2022-06-11 00:11:08 +0100

    created customer entity

---
+++ src/main/java/com/example/crm/entities/Customer.java
@@ -0,0 +1,24 @@
+package com.example.crm.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Table(name ="customer")
+
+public class Customer{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+
+    private Long Id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
